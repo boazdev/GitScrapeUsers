204f9664ae033a6208fccae95dd9bd59ab4f5b69
boazdev <boaz.sadeh2@gmail.com>
2023-09-21 20:14:29 +0300

    .env

--- app/routers/users_router.py
+++ app/routers/users_router.py
@@ -1,11 +1,13 @@
-from fastapi import APIRouter, Depends,HTTPException
+from fastapi import APIRouter, Depends,HTTPException, Response
+from fastapi.responses import StreamingResponse
 from sqlalchemy.orm import Session
 from fastapi.responses import JSONResponse
-from ..utils.github_utils import create_github_headers,create_github_url, users_from_json
-from ..service.github_service import get_users_by_url
+from app.utils.github_utils import create_github_headers,create_github_url, users_from_json
+from app.service.github_service import get_users_by_url
 router = APIRouter(prefix="/users", tags=["users"])
 from app.schemas.options_schema import OptionsIn
-
+import anyio
+import time
 @router.get("/", response_model=dict, status_code=200)
 def get_github_users():
     return {"users":[1,2,3,4,5]}
@@ -20,6 +22,22 @@ def save_github_users(options: OptionsIn):
     users_lst = users_from_json(users_json)
     print(users_lst)
     return {"users_added":100,"users_current":300}
+
+async def fake_video_streamer():
+    for i in range(1, 11):
+        await anyio.sleep(2)
+        yield f"Data point {i}\n"
+        #await asyncio.sleep(1)  # Simulate some asynchronous work
+
+def fake_data_streamer():
+    for i in range(10):
+        yield b'some fake data\n\n'
+        time.sleep(1.0)
+        
+
+@router.get("/stream-data")
+async def stream_data():
+    return StreamingResponse(fake_video_streamer(), media_type='text/event-stream')
 
 """ 
 @router.post("/createUser", response_model=UserSchema.BaseUser)
--- app/schemas/options_schema.py
+++ app/schemas/options_schema.py
@@ -6,7 +6,7 @@ class OptionsIn(BaseModel):
     The minimum number of repositories a user must have to be added to the database.
     Default is 5.
     """
-    delay : int = 0
-    max_users : int = 10000
+    delay : int = 0 #delay between requests to github
+    max_users : int = 10000  #maxium number of users to scan
 
 
